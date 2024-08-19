97613c31ed68b1dcfa322717d407f5c81ce6cc56
Lifan Sun <lifansun1412@gmail.com>
2024-08-19 15:08:50 +0800

    formatting files

--- model.py
+++ model.py
@@ -14,14 +14,18 @@ class PointWiseFeedForward(torch.nn.Module):
         self.dropout2 = torch.nn.Dropout(p=dropout_rate)
 
     def forward(self, inputs):
-        outputs = self.dropout2(self.conv2(self.relu(self.dropout1(self.conv1(inputs.transpose(-1, -2))))))
-        outputs = outputs.transpose(-1, -2) # as Conv1D requires (N, C, Length)
+        outputs = self.dropout2(
+            self.conv2(self.relu(self.dropout1(self.conv1(inputs.transpose(-1, -2)))))
+        )
+        outputs = outputs.transpose(-1, -2)  # as Conv1D requires (N, C, Length)
         outputs += inputs
         return outputs
+
 
 # pls use the following self-made multihead attention layer
 # in case your pytorch version is below 1.16 or for other reasons
 # https://github.com/pmixer/TiSASRec.pytorch/blob/master/model.py
+
 
 class SASRec(torch.nn.Module):
     def __init__(self, user_num, item_num, args):
@@ -33,11 +37,15 @@ class SASRec(torch.nn.Module):
 
         # TODO: loss += args.l2_emb for regularizing embedding vectors during training
         # https://stackoverflow.com/questions/42704283/adding-l1-l2-regularization-in-pytorch
-        self.item_emb = torch.nn.Embedding(self.item_num+1, args.hidden_units, padding_idx=0)
-        self.pos_emb = torch.nn.Embedding(args.maxlen+1, args.hidden_units, padding_idx=0)
+        self.item_emb = torch.nn.Embedding(
+            self.item_num + 1, args.hidden_units, padding_idx=0
+        )
+        self.pos_emb = torch.nn.Embedding(
+            args.maxlen + 1, args.hidden_units, padding_idx=0
+        )
         self.emb_dropout = torch.nn.Dropout(p=args.dropout_rate)
 
-        self.attention_layernorms = torch.nn.ModuleList() # to be Q for self-attention
+        self.attention_layernorms = torch.nn.ModuleList()  # to be Q for self-attention
         self.attention_layers = torch.nn.ModuleList()
         self.forward_layernorms = torch.nn.ModuleList()
         self.forward_layers = torch.nn.ModuleList()
@@ -48,9 +56,9 @@ class SASRec(torch.nn.Module):
             new_attn_layernorm = torch.nn.LayerNorm(args.hidden_units, eps=1e-8)
             self.attention_layernorms.append(new_attn_layernorm)
 
-            new_attn_layer =  torch.nn.MultiheadAttention(args.hidden_units,
-                                                            args.num_heads,
-                                                            args.dropout_rate)
+            new_attn_layer = torch.nn.MultiheadAttention(
+                args.hidden_units, args.num_heads, args.dropout_rate
+            )
             self.attention_layers.append(new_attn_layer)
 
             new_fwd_layernorm = torch.nn.LayerNorm(args.hidden_units, eps=1e-8)
@@ -62,36 +70,39 @@ class SASRec(torch.nn.Module):
             # self.pos_sigmoid = torch.nn.Sigmoid()
             # self.neg_sigmoid = torch.nn.Sigmoid()
 
-    def log2feats(self, log_seqs): # TODO: fp64 and int64 as default in python, trim?
+    def log2feats(self, log_seqs):  # TODO: fp64 and int64 as default in python, trim?
         seqs = self.item_emb(torch.LongTensor(log_seqs).to(self.dev))
-        seqs *= self.item_emb.embedding_dim ** 0.5
+        seqs *= self.item_emb.embedding_dim**0.5
         poss = np.tile(np.arange(1, log_seqs.shape[1] + 1), [log_seqs.shape[0], 1])
         # TODO: directly do tensor = torch.arange(1, xxx, device='cuda') to save extra overheads
-        poss *= (log_seqs != 0)
+        poss *= log_seqs != 0
         seqs += self.pos_emb(torch.LongTensor(poss).to(self.dev))
         seqs = self.emb_dropout(seqs)
 
-        tl = seqs.shape[1] # time dim len for enforce causality
-        attention_mask = ~torch.tril(torch.ones((tl, tl), dtype=torch.bool, device=self.dev))
+        tl = seqs.shape[1]  # time dim len for enforce causality
+        attention_mask = ~torch.tril(
+            torch.ones((tl, tl), dtype=torch.bool, device=self.dev)
+        )
 
         for i in range(len(self.attention_layers)):
             seqs = torch.transpose(seqs, 0, 1)
             Q = self.attention_layernorms[i](seqs)
-            mha_outputs, _ = self.attention_layers[i](Q, seqs, seqs, 
-                                            attn_mask=attention_mask)
-                                            # need_weights=False) this arg do not work?
+            mha_outputs, _ = self.attention_layers[i](
+                Q, seqs, seqs, attn_mask=attention_mask
+            )
+            # need_weights=False) this arg do not work?
             seqs = Q + mha_outputs
             seqs = torch.transpose(seqs, 0, 1)
 
             seqs = self.forward_layernorms[i](seqs)
             seqs = self.forward_layers[i](seqs)
 
-        log_feats = self.last_layernorm(seqs) # (U, T, C) -> (U, -1, C)
+        log_feats = self.last_layernorm(seqs)  # (U, T, C) -> (U, -1, C)
 
         return log_feats
 
-    def forward(self, user_ids, log_seqs, pos_seqs, neg_seqs): # for training        
-        log_feats = self.log2feats(log_seqs) # user_ids hasn't been used yet
+    def forward(self, user_ids, log_seqs, pos_seqs, neg_seqs):  # for training
+        log_feats = self.log2feats(log_seqs)  # user_ids hasn't been used yet
 
         pos_embs = self.item_emb(torch.LongTensor(pos_seqs).to(self.dev))
         neg_embs = self.item_emb(torch.LongTensor(neg_seqs).to(self.dev))
@@ -102,24 +113,28 @@ class SASRec(torch.nn.Module):
         # pos_pred = self.pos_sigmoid(pos_logits)
         # neg_pred = self.neg_sigmoid(neg_logits)
 
-        return pos_logits, neg_logits # pos_pred, neg_pred
+        return pos_logits, neg_logits  # pos_pred, neg_pred
 
-    def predict(self, user_ids, log_seqs, item_indices,topk=20): # for inference
-        #(B, L, C)
-        log_feats = self.log2feats(log_seqs) # user_ids hasn't been used yet
-        
-        #(B, C)
-        final_feat = log_feats[:, -1, :] # only use last QKV classifier, a waste
-        
+    def predict(self, user_ids, log_seqs, item_indices, topk=20):  # for inference
+        # (B, L, C)
+        log_feats = self.log2feats(log_seqs)  # user_ids hasn't been used yet
+
+        # (B, C)
+        final_feat = log_feats[:, -1, :]  # only use last QKV classifier, a waste
+
         # (num_items, C)
-        item_embs = self.item_emb(torch.LongTensor(item_indices).to(self.dev)) # (U, I, C)
-        
+        item_embs = self.item_emb(
+            torch.LongTensor(item_indices).to(self.dev)
+        )  # (U, I, C)
+
         # (B, num_items)
         logits = item_embs.matmul(final_feat.unsqueeze(-1)).squeeze(-1)
 
         # preds = self.pos_sigmoid(logits) # rank same item list for different users
         topk_indices = None
         if topk > 0:
-            _, topk_indices = torch.topk(logits, topk, dim=-1, largest=True, sorted=True)
+            _, topk_indices = torch.topk(
+                logits, topk, dim=-1, largest=True, sorted=True
+            )
 
-        return logits, topk_indices # preds # (U, I)
+        return logits, topk_indices  # preds # (U, I)
--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
 import sys
 import copy
-import torch
 import random
-import json
 import numpy as np
 from collections import defaultdict
 from multiprocessing import Process, Queue
 from typing import List, Tuple, Dict
 
-def build_index(dataset_name: str, meta_info: Dict[str, int]=None) -> Tuple[List[List[int]], List[List[int]]]:
+
+def build_index(
+    dataset_name: str, meta_info: Dict[str, int] = None
+) -> Tuple[List[List[int]], List[List[int]]]:
     """
     Build user to item and item to user relationship.
     Args:
@@ -18,14 +19,14 @@ def build_index(dataset_name: str, meta_info: Dict[str, int]=None) -> Tuple[List
         u2i_index: user item relationship, specify the items each user interacted with.
         i2u_index: item user relationship, specify the users each item was interacted by.
     """
-    ui_mat = np.loadtxt('data/%s.txt' % dataset_name, dtype=np.int32)
+    ui_mat = np.loadtxt("data/%s.txt" % dataset_name, dtype=np.int32)
 
     n_users = ui_mat[:, 0].max()
     n_items = ui_mat[:, 1].max()
-    
+
     if meta_info:
-        n_users = max(n_users, meta_info['num_users'])
-        n_items = max(n_items, meta_info['num_items'])
+        n_users = max(n_users, meta_info["num_users"])
+        n_items = max(n_items, meta_info["num_items"])
 
     u2i_index = [[] for _ in range(n_users + 1)]
     i2u_index = [[] for _ in range(n_items + 1)]
@@ -36,20 +37,23 @@ def build_index(dataset_name: str, meta_info: Dict[str, int]=None) -> Tuple[List
 
     return u2i_index, i2u_index
 
+
 # sampler for batch generation
-def random_neq(l, r, s):
+def random_neq(l: int, r: int, s: list[int]) -> int:
     t = np.random.randint(l, r)
     while t in s:
         t = np.random.randint(l, r)
     return t
 
 
-def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
+def sample_function(
+    user_train, usernum, itemnum, batch_size, maxlen, result_queue, SEED
+):
     def sample(uid):
 
         # uid = np.random.randint(1, usernum + 1)
         # while len(user_train[uid]) <= 1: uid = np.random.randint(1, usernum + 1)
-        while uid not in user_train or len(user_train[uid]) < 1:
+        while uid not in user_train or len(user_train[uid]) <= 1:
             uid = random.choice(list(user_train.keys()))
 
         seq = np.zeros([maxlen], dtype=np.int32)
@@ -62,21 +66,23 @@ def sample_function(user_train, usernum, itemnum, batch_size, maxlen, result_que
         for i in reversed(user_train[uid][:-1]):
             seq[idx] = i
             pos[idx] = nxt
-            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
+            if nxt != 0:
+                neg[idx] = random_neq(1, itemnum + 1, ts)
             nxt = i
             idx -= 1
-            if idx == -1: break
+            if idx == -1:
+                break
 
         return (uid, seq, pos, neg)
 
     np.random.seed(SEED)
-    uids = np.arange(1, usernum+1, dtype=np.int32)
+    uids = np.arange(1, usernum + 1, dtype=np.int32)
     counter = 0
     while True:
         if counter % usernum == 0:
             np.random.shuffle(uids)
         one_batch = []
-        for i in range(batch_size):
+        for _ in range(batch_size):
             one_batch.append(sample(uids[counter % usernum]))
             counter += 1
         result_queue.put(zip(*one_batch))
@@ -88,14 +94,19 @@ class WarpSampler(object):
         self.processors = []
         for i in range(n_workers):
             self.processors.append(
-                Process(target=sample_function, args=(User,
-                                                      usernum,
-                                                      itemnum,
-                                                      batch_size,
-                                                      maxlen,
-                                                      self.result_queue,
-                                                      np.random.randint(2e9)
-                                                      )))
+                Process(
+                    target=sample_function,
+                    args=(
+                        User,
+                        usernum,
+                        itemnum,
+                        batch_size,
+                        maxlen,
+                        self.result_queue,
+                        np.random.randint(2e9),
+                    ),
+                )
+            )
             self.processors[-1].daemon = True
             self.processors[-1].start()
 
@@ -117,9 +128,9 @@ def data_partition(fname):
     user_valid = {}
     user_test = {}
     # assume user/item index starting from 1
-    f = open('data/%s.txt' % fname, 'r')
+    f = open("data/%s.txt" % fname, "r")
     for line in f:
-        u, i = line.rstrip().split(' ')
+        u, i = line.rstrip().split(" ")
         u = int(u)
         i = int(i)
         usernum = max(u, usernum)
@@ -140,134 +151,14 @@ def data_partition(fname):
             user_test[user].append(User[user][-1])
     return [user_train, user_valid, user_test, usernum, itemnum]
 
+
 # TODO: merge evaluate functions for test and val set
-# evaluate on test set
-# def evaluate(model, dataset, args):
-#     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
-#     NDCG = 0.0
-#     HT = 0.0
-#     valid_user = 0.0
-#     ### 初始化 topk dict ###
-#     topk_dict = {}
-#     ### 初始化 topk dict ###
-
-#     #if usernum>10000:
-#     #    users = random.sample(range(1, usernum + 1), 10000)
-#     #else:
-#     #    users = range(1, usernum + 1)
-#     users = range(1, usernum + 1)
-#     for u in users:
-
-#         if u not in train or len(train[u]) < 1 or u not in test or len(test[u]) < 1: continue
-
-#         seq = np.zeros([args.maxlen], dtype=np.int32)
-#         idx = args.maxlen - 1
-#         seq[idx] = valid[u][0]
-#         idx -= 1
-#         for i in reversed(train[u]):
-#             seq[idx] = i
-#             idx -= 1
-#             if idx == -1: break
-#         rated = set(train[u])
-#         rated.add(0)
-#         item_idx = [test[u][0]]
-#         for _ in range(100):
-#             t = np.random.randint(1, itemnum + 1)
-#             while t in rated: t = np.random.randint(1, itemnum + 1)
-#             item_idx.append(t)
-
-#         predictions, topk_indices = model.predict(*[np.array(l) for l in [[u], [seq], item_idx]], topk=args.save_topk)
-#         predictions = -predictions
-#         predictions = predictions[0] # - for 1st argsort DESC
-
-#         rank = predictions.argsort().argsort()[0].item()
-
-#         valid_user += 1
-
-#         if rank < 10:
-#             NDCG += 1 / np.log2(rank + 2)
-#             HT += 1
-
-#         ### 保存 top-k 推荐结果 ###
-#         topk_items = [item_idx[i] for i in topk_indices[0].cpu().numpy()]
-#         interaction_history = [i for i in train[u]]
-#         topk_dict[u] = (interaction_history, topk_items)
-#         ### 保存 top-k 推荐结果 ###
-        
-#         if valid_user % 100 == 0:
-#             print('.', end="")
-#             sys.stdout.flush()
-#     ### 保存 top-k 推荐结果到 CSV 文件 ###
-#     with open('topk_recommendations.csv', 'w', newline='') as csvfile:
-#         csvwriter = csv.writer(csvfile)
-#         csvwriter.writerow(['user_id', 'interaction_history', 'recommendations'])
-#         for user, (history, recommendations) in topk_dict.items():
-#             csvwriter.writerow([user, ' '.join(map(str, history)), ' '.join(map(str, recommendations))])
-#     ### 保存 top-k 推荐结果到 CSV 文件 ###
-
-#     return NDCG / valid_user, HT / valid_user
-
-# def evaluate(model, dataset, args):
-#     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
-#     NDCG = 0.0
-#     HT = 0.0
-#     valid_user = 0.0
-#     topk_dict = {}
-
-#     users = range(1, usernum + 1)
-#     for u in users:
-#         if u not in train or len(train[u]) < 1 or u not in test or len(test[u]) < 1:
-#             continue
-
-#         # Prepare sequence for prediction
-#         seq = np.zeros([args.maxlen], dtype=np.int32)
-#         idx = args.maxlen - 1
-#         seq[idx] = valid[u][0]
-#         idx -= 1
-#         for i in reversed(train[u]):
-#             seq[idx] = i
-#             idx -= 1
-#             if idx == -1:
-#                 break
-#         rated = set(train[u])
-#         rated.add(0)
-#         item_idx = [test[u][0]]
-#         for _ in range(100):
-#             t = np.random.randint(1, itemnum + 1)
-#             while t in rated:
-#                 t = np.random.randint(1, itemnum + 1)
-#             item_idx.append(t)
-
-#         # Model prediction
-#         predictions, topk_indices = model.predict(*[np.array(l) for l in [[u], [seq], item_idx]], topk=args.save_topk)
-#         predictions = -predictions
-#         predictions = predictions[0]
-#         rank = predictions.argsort().argsort()[0].item()
-
-#         valid_user += 1
-
-#         if rank < 10:
-#             NDCG += 1 / np.log2(rank + 2)
-#             HT += 1
-
-#         if valid_user % 100 == 0:
-#             print('.', end="")
-#             sys.stdout.flush()
-
-#     return NDCG / valid_user, HT / valid_user
-
-def evaluate(model, dataset, args, save_path='evaluation_results.json'):
+def evaluate(model, dataset, args):
     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
 
     NDCG = 0.0
     HT = 0.0
     valid_user = 0.0
-    topk_dict = {}
-
-    # 用于保存每个用户的历史记录和推荐结果
-    results = {}
 
     users = range(1, usernum + 1)
     for u in users:
@@ -294,7 +185,9 @@ def evaluate(model, dataset, args, save_path='evaluation_results.json'):
             item_idx.append(t)
 
         # Model prediction
-        predictions, topk_indices = model.predict(*[np.array(l) for l in [[u], [seq], item_idx]], topk=args.save_topk)
+        predictions, _ = model.predict(
+            *[np.array(l) for l in [[u], [seq], item_idx]], topk=args.save_topk
+        )
         predictions = -predictions
         predictions = predictions[0]
         rank = predictions.argsort().argsort()[0].item()
@@ -305,25 +198,11 @@ def evaluate(model, dataset, args, save_path='evaluation_results.json'):
             NDCG += 1 / np.log2(rank + 2)
             HT += 1
 
-        # 保存每个用户的历史记录和推荐结果
-        if args.save_topk > 0:
-            results[u] = {
-                'sequence': seq.tolist(),
-                'topk_recommendations': [item_idx[idx] for idx in topk_indices[0].tolist()],
-                'true_item': test[u][0]
-            }
-
         if valid_user % 100 == 0:
-            print('.', end="")
+            print(".", end="")
             sys.stdout.flush()
-    
-    if args.save_topk > 0: 
-        # 将结果保存到文件中
-        with open(save_path, 'w') as f:
-            json.dump(results, f)
 
     return NDCG / valid_user, HT / valid_user
-
 
 
 # evaluate on val set
@@ -333,29 +212,34 @@ def evaluate_valid(model, dataset, args):
     NDCG = 0.0
     valid_user = 0.0
     HT = 0.0
-    if usernum>10000:
+    if usernum > 10000:
         users = random.sample(range(1, usernum + 1), 10000)
     else:
         users = range(1, usernum + 1)
     for u in users:
-        if u not in train or len(train[u]) < 1 or u not in valid or len(valid[u]) < 1: continue
+        if u not in train or len(train[u]) < 1 or u not in valid or len(valid[u]) < 1:
+            continue
 
         seq = np.zeros([args.maxlen], dtype=np.int32)
         idx = args.maxlen - 1
         for i in reversed(train[u]):
             seq[idx] = i
             idx -= 1
-            if idx == -1: break
+            if idx == -1:
+                break
 
         rated = set(train[u])
         rated.add(0)
         item_idx = [valid[u][0]]
         for _ in range(100):
             t = np.random.randint(1, itemnum + 1)
-            while t in rated: t = np.random.randint(1, itemnum + 1)
+            while t in rated:
+                t = np.random.randint(1, itemnum + 1)
             item_idx.append(t)
 
-        predictions, _ = model.predict(*[np.array(l) for l in [[u], [seq], item_idx]], topk=-1)
+        predictions, _ = model.predict(
+            *[np.array(l) for l in [[u], [seq], item_idx]], topk=-1
+        )
         predictions = -predictions
         predictions = predictions[0]
 
@@ -367,7 +251,7 @@ def evaluate_valid(model, dataset, args):
             NDCG += 1 / np.log2(rank + 2)
             HT += 1
         if valid_user % 100 == 0:
-            print('.', end="")
+            print(".", end="")
             sys.stdout.flush()
 
     return NDCG / valid_user, HT / valid_user
